1731a272296a31cef7b08ad9c474f9de31e787d9
dim2k2006 <dim2k2006@yandex.ru>
2014-06-16 16:42:19 +0300

    3 функции

--- rtrim.php
+++ rtrim.php
@@ -8,9 +8,15 @@ var_dump($trimmed);
 
 echo "<br><br><br>";
 
+
+/**
+*Удаляет пробелы у строки справа
+*@param string $text
+*@return string
+*/
 function my_rtrim ($text) {
-	$string = str_split($text);
-	
+	$string = my_str_split ($text);
+
 	for ($i = count($string) - 1; $i > 0 ; $i--) { 
 		if ($string[$i] == " ") {
 			unset($string[$i]);
@@ -19,8 +25,40 @@ function my_rtrim ($text) {
 		}
 	}
 	
-	return $newString = implode("", $string);
+	return $newString = my_implode ($string, $separator = "");
 }
+
+
+/**
+*Преобразует строку в массив
+*@param string $string
+*@return array
+*/
+function my_str_split ($string) {
+	$stringLength = strlen($string);
+	$array = array();
+	for ($i=0; $i < $stringLength; $i++) { 
+		array_push($array, $string{$i});
+	}
+	return $array;
+}
+
+
+/**
+*Объединяет элементы массива в строку
+*@param array $array
+*@param string $separator
+*@return string
+*/
+function my_implode (array $array, $separator) {
+	$html ="";
+	for ($i=0; $i < count($array) ; $i++) { 
+		$html .= $array[$i].$separator;
+	}
+	return $html;
+}
+
+
 
 
 $myTrimmed = my_rtrim ($text);
